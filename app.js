905f6c212cdfabc784907f009d64df7aa8141f9c
maurilop92 <maurilop92@gmail.com>
2016-06-04 02:22:26 -0300

    Separated cat functionality from VM

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Kitty');
   this.nickname = ko.observable('Baby');
   this.imgSrc = ko.observable('img/cat1.jpg');
   this.imgAttribution = ko.observable('none');
+  this.friends = ko.observableArray([{name: 'Peter'}, {name: 'Gary'}, {name: 'Nick'}, {name: 'Sam'}]);
+
+  this.nickname = ko.computed(function() {
+    var nickname;
+    var clicks = this.clickCount();
+    if (clicks < 10) {
+      nickname = 'Baby';
+    } else if (clicks < 50) {
+      nickname = 'Young';
+    } else {
+      nickname = 'Adult';
+    }
+    return nickname;
+  }, this);
+};
+
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
 
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-
-    if(this.clickCount() > 10) {
-      return this.nickname('Adult');
-    }else if(this.clickCount() > 5) {
-      return this.nickname('Young');
-    }else{
-      return this.nickname('Baby');
-    }
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   };
 };
 
